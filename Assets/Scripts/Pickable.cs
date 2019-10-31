f5e8b70643a9417caf27bf66ea70dd257f371940
Marco Secchi <loziosecchi@gmail.com>
2019-10-31 10:42:37 +0100

    Add trigger example.

---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+
+    public float jumpBonus = 20;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger enter: " + collision.gameObject.name);
+
+        GameObject go = collision.gameObject;
+        RobotController robotController = go.GetComponent<RobotController>();
+        if(robotController != null)
+        {
+            robotController.jumpMultiplier = jumpBonus;
+        }
+
+        Destroy(gameObject);
+    }
+
+}
