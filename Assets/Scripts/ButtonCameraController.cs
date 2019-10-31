7cef576e7918e6f231dcc1665b600ab1a4ece2c7
Marco Secchi <loziosecchi@gmail.com>
2019-10-31 12:37:15 +0100

    Update Cinemachine examples.

---
+++ Assets/Scripts/ButtonCameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ButtonCameraController : MonoBehaviour
+{
+
+    public CinemachineVirtualCamera shot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool cameraEnabled = Input.GetKey(KeyCode.Alpha1);
+        shot.gameObject.SetActive(cameraEnabled);
+    }
+}
